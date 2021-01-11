68be886be08dcba9fd6e5d546ff02d4cf7c8512e
Aidan Mundy <amboyscout@aol.com>
2021-01-11 14:37:05 -0500

    New subm

---
+++ src/simpleplayer2_subm/DirectionUtils.java
@@ -0,0 +1,83 @@
+package simpleplayer2_subm;
+import battlecode.common.Direction;
+
+public class DirectionUtils {
+    public static final Direction[] nonCenterDirections = {
+        Direction.NORTH,
+        Direction.NORTHEAST,
+        Direction.EAST,
+        Direction.SOUTHEAST,
+        Direction.SOUTH,
+        Direction.SOUTHWEST,
+        Direction.WEST,
+        Direction.NORTHWEST,
+    };
+
+    public static Direction randomDirection() {
+        return nonCenterDirections[(int) (Math.random() * nonCenterDirections.length)];
+    }
+
+    public static Direction rotateRight90(Direction dir) {
+        return dir.rotateRight().rotateRight();
+    }
+
+    public static Direction rotateLeft90(Direction dir) {
+        return dir.rotateLeft().rotateLeft();
+    }
+
+    public static Direction rotateRightSnapCardinal(Direction dir){
+        switch(dir){
+            case NORTH: return Direction.EAST;
+            case NORTHEAST: return Direction.EAST;
+            case EAST: return Direction.SOUTH;
+            case SOUTHEAST: return Direction.SOUTH;
+            case SOUTH: return Direction.WEST;
+            case SOUTHWEST: return Direction.WEST;
+            case WEST: return Direction.NORTH;
+            case NORTHWEST: return Direction.NORTH;
+            default: return Direction.CENTER;
+        }
+    }
+
+    public static Direction rotateLeftSnapCardinal(Direction dir){
+        switch(dir){
+            case NORTH: return Direction.WEST;
+            case NORTHEAST: return Direction.NORTH;
+            case EAST: return Direction.NORTH;
+            case SOUTHEAST: return Direction.EAST;
+            case SOUTH: return Direction.EAST;
+            case SOUTHWEST: return Direction.SOUTH;
+            case WEST: return Direction.SOUTH;
+            case NORTHWEST: return Direction.WEST;
+            default: return Direction.CENTER;
+        }
+    }
+
+    public static Direction rotateRightSnapDiagonal(Direction dir){
+        switch(dir){
+            case NORTH: return Direction.NORTHEAST;
+            case NORTHEAST: return Direction.SOUTHEAST;
+            case EAST: return Direction.SOUTHEAST;
+            case SOUTHEAST: return Direction.SOUTHWEST;
+            case SOUTH: return Direction.SOUTHWEST;
+            case SOUTHWEST: return Direction.NORTHWEST;
+            case WEST: return Direction.NORTHWEST;
+            case NORTHWEST: return Direction.NORTHEAST;
+            default: return Direction.CENTER;
+        }
+    }
+
+    public static Direction rotateLeftSnapDiagonal(Direction dir){
+        switch(dir){
+            case NORTH: return Direction.NORTHWEST;
+            case NORTHEAST: return Direction.NORTHWEST;
+            case EAST: return Direction.NORTHEAST;
+            case SOUTHEAST: return Direction.NORTHEAST;
+            case SOUTH: return Direction.SOUTHEAST;
+            case SOUTHWEST: return Direction.SOUTHEAST;
+            case WEST: return Direction.SOUTHWEST;
+            case NORTHWEST: return Direction.SOUTHWEST;
+            default: return Direction.CENTER;
+        }
+    }
+}
---
+++ src/simpleplayer2_subm/Move.java
@@ -0,0 +1,43 @@
+package simpleplayer2_subm;
+
+import battlecode.common.*;
+
+public class Move {
+    static boolean tryMove(RobotController rc, Direction dir) throws GameActionException {
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+            return true;
+        }
+        return false;
+    }
+
+    static Direction dirForward180(RobotController rc, Direction dir) throws GameActionException{
+        if(rc.canMove(dir) || !rc.onTheMap(rc.getLocation().add(dir))){
+            return dir;
+        } else if (rc.canMove(dir.rotateLeft())){
+            return dir.rotateLeft();
+        } else if (rc.canMove(dir.rotateRight())){
+            return dir.rotateRight();
+        } else if (rc.canMove(DirectionUtils.rotateLeft90(dir))){
+            return DirectionUtils.rotateLeft90(dir);
+        } else if (rc.canMove(DirectionUtils.rotateRight90(dir))){
+            return DirectionUtils.rotateRight90(dir);
+        }
+        return dir;
+    }
+
+    static Direction dirForward90(RobotController rc, Direction dir) throws GameActionException{
+        if(rc.canMove(dir) || !rc.onTheMap(rc.getLocation().add(dir))){
+            return dir;
+        } else if (rc.canMove(dir.rotateLeft())){
+            return dir.rotateLeft();
+        } else if (rc.canMove(dir.rotateRight())){
+            return dir.rotateRight();
+        }
+        return dir;
+    }
+
+    static Direction getTeamGoDir(RobotController robot){
+        return robot.getTeam().equals(Team.A) ? Direction.WEST : Direction.EAST; //TODO: make this smarter than assuming team a is on the left side
+    }
+}
