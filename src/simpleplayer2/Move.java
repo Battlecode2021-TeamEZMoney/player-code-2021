305dbffe8411c239a60393d87330e7e392c1d90d
Kim <77079059+tatatatatatatatapooooooo@users.noreply.github.com>
2021-01-11 19:18:15 -0500

    Added a potentially improved dirForward90 method in block comments

--- src/simpleplayer2/Move.java
+++ src/simpleplayer2/Move.java
@@ -37,7 +37,40 @@ public class Move {
         }
         return dir;
     }
-
+    
+    /*
+    static Direction dirForward90(RobotController rc, Direction dir) throws GameActionException{
+        double[] movenesses = [trySenseMoveness(dir.rotateLeft()), trySenseMoveness(dir), trySenseMoveness(dir.rotateRight())]
+        switch (dir){
+            case NORTH:
+            case EAST:
+            case SOUTH:
+            case WEST:
+                if ( (movenesses[1] > movenesses[0]) && (movenesses[1] > movenesses[2]) )
+                    return dir;
+                else if (movenesses[0] > movenesses[2])
+                    return dir.rotateLeft();
+                else if (movenesses[2] > 0.0)
+                    return dir.rotateRight();
+                return Direction.CENTER;
+            default:
+                if ( (movenesses[1] > movenesses[0] * 0.5) && (movenesses[1] > movenesses[2] * 0.5) )
+                    return dir;
+                else if (movenesses[0] > movenesses[2])
+                    return dir.rotateLeft();
+                else if (movenesses[2] > 0.0)
+                    return dir.rotateRight();
+                return Direction.CENTER;
+        }
+    }
+    
+    double trySenseMoveness(Direction dir){
+        if canMove(dir)
+            return this.sensePassability(this.adjacentLocation(dir));
+        else return 0.0;
+    }
+    */
+    
     static Direction getTeamGoDir(RobotController robot){
         return robot.getTeam().equals(Team.A) ? Direction.WEST : Direction.EAST; //TODO: make this smarter than assuming team a is on the left side
     }
