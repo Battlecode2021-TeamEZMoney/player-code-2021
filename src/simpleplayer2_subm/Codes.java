65bcc12f2a971b74a44acd360418bb7e91c364f6
Aidan Mundy <amboyscout@aol.com>
2021-01-13 16:52:05 -0500

    made a subm

---
+++ src/simpleplayer2_subm/Codes.java
@@ -0,0 +1,9 @@
+package simpleplayer2_subm;
+
+public class Codes {
+    public static int simple = 1;
+    public static int enemyHQ = 2;
+    public static int friendlyHQ = 3;
+    public static int neutralHQ = 4;
+    public static int patrol = 5;
+}
---
+++ src/simpleplayer2_subm/RunAway.java
@@ -0,0 +1,16 @@
+package simpleplayer2_subm;
+
+import java.util.ArrayList;
+
+import battlecode.common.*;
+
+public class RunAway {
+	static Direction runAwayDirection(RobotController rc, ArrayList<RobotInfo> robots) {
+		MapLocation location = new MapLocation(0,0);
+		for (RobotInfo robot : robots) {
+			location = location.translate(robot.location.x, robot.location.y);
+		}
+		MapLocation avgLocation = new MapLocation(location.x/robots.size(), location.y/robots.size());
+		return rc.getLocation().directionTo(avgLocation).opposite();
+	}
+}
