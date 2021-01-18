e1e9e8fe5b7867719492c7b2aadae04a624e2bb2
Kim <77079059+tatatatatatatatapooooooo@users.noreply.github.com>
2021-01-17 21:25:54 -0500

    i'll get back to this after dinner

--- src/simpleplayer3/Pathing.java
+++ src/simpleplayer3/Pathing.java
@@ -4,5 +4,28 @@ import battlecode.common.*;
 import common.DirectionUtils;
 
 public abstract strictfp class Pathing {
-
+  
+  public static Direction pathingDir( RobotController rc, MapLocation dest ) {
+    MapLocation here = rc.getLocation;
+    Direction wayTo = here.directionTo( dest );
+    
+    if rc.canSenseLocation( dest ) {
+      if ( here.isWithinDistanceSquared( dest, 2 ) ) return center;
+      
+    }
+    
+    double[] speeds = { 0.0, 0.0, 0.0 };
+    Direction[] dirs;
+    
+    switch wayTo {
+      case Direction.NORTH:
+      case Direction.WEST:
+      case Direction.EAST:
+      case Direction.SOUTH:
+        
+      default:
+        
+    }
+    
+  }
 }
