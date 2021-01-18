58d35fd2f7866d966b0d329f958a45a1b8179754
Vittal Thirumalai <Vittal.Thirumalai@warriorlife.net>
2021-01-18 15:58:11 -0800

    4_sprint2

---
+++ src/simpleplayer4_sprint2/Attacker.java
@@ -0,0 +1,18 @@
+package simpleplayer4_sprint2;
+
+import battlecode.common.*;
+
+public abstract class Attacker extends Pawn {
+    Attacker(RobotController rcin) throws GameActionException {
+        super(rcin); // Don't remove this.
+    }
+    protected boolean withinAttackRange(RobotInfo enemy) throws GameActionException {
+        return withinAttackRange(enemy.getLocation());
+    }
+
+    protected boolean withinAttackRange(MapLocation enemyLocation) throws GameActionException {
+        return rc.getLocation().distanceSquaredTo(enemyLocation) <= actionRadiusSquared;
+    }
+
+    protected abstract boolean huntOrKill(RobotInfo enemy) throws GameActionException;
+}
