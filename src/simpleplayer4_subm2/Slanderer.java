3993ac0e91533a9b9b2cd00abc9dc7a5ed72174f
Vittal Thirumalai <Vittal.Thirumalai@warriorlife.net>
2021-01-17 21:49:15 -0800

    Created 4_subm2

---
+++ src/simpleplayer4_subm2/Slanderer.java
@@ -0,0 +1,29 @@
+package simpleplayer4_subm2;
+
+import battlecode.common.*;
+//import java.util.*;
+
+class Slanderer extends Pawn {
+    Politician successor;
+
+    Slanderer(RobotController rcin) throws GameActionException {
+        this.rc = rcin;
+        getHomeHQ();
+    }
+
+    void run() throws GameActionException {
+        while (rc.getType().equals(RobotType.SLANDERER)) {
+            turnCount++;
+            if (rc.isReady()) {
+                // Handle detecting enemies
+            	tryDirForward180(awayFromEnemyMuckrakers());
+            }
+            Clock.yield();
+        }
+        if (successor == null) {
+            successor = new Politician(this);
+        }
+        successor.run();
+
+    }
+}
